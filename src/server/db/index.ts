ef77fd06d3036a22eeb9124b87d5573153131878
mrg722 <mar.reyesg@duocuc.cl>
2026-09-22 18:29:36 -0300

    fix: use webpack-compatible Node builtin imports in db runtime

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,6 @@
 import 'server-only';
-import { mkdir } from 'node:fs/promises';
-import { dirname } from 'node:path';
+import { mkdir } from 'fs/promises';
+import { dirname } from 'path';
 import { env } from '../env';
 
 /**
