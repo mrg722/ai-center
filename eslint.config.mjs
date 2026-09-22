4cc3b2e388cde9f2741b811d5b494bbf4bc96b2c
mrg722 <mar.reyesg@duocuc.cl>
2026-09-22 16:55:08 -0300

    chore: lint Next.js rules in the flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
+import nextPlugin from '@next/eslint-plugin-next';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -11,9 +12,10 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
-    plugins: { 'react-hooks': reactHooks },
+    plugins: { 'react-hooks': reactHooks, '@next/next': nextPlugin },
     rules: {
       'react-hooks/exhaustive-deps': 'warn',
+      ...nextPlugin.configs.recommended.rules,
     },
   },
   {
